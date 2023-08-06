33de2553271f3ee89e1711b8d4c560f491578c17
Thomas Cousin <907403+Tomcuzz@users.noreply.github.com>
2023-08-06 00:41:44 +0000

    Updates to fix refrsh to int

--- src/pages/photo.py
+++ src/pages/photo.py
@@ -3,7 +3,7 @@
 def add_photo_page(app):
     """ Add Home Page """
     @app.route("/photo")
-    @app.route('/photo/<refresh>')
+    @app.route('/photo/<int:refresh>')
     def photo_page(refresh=900):
         """ Photo Page """
-        return "<p>Photo page to go here!</p>"
+        return "<p>Photo page to go here! <br> Refresh is: " + str(refresh) + "</p>"

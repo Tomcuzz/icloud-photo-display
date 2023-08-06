8acd22df982bfd0bf8028d015b2eaea9d5630977
Thomas Cousin <907403+Tomcuzz@users.noreply.github.com>
2023-08-06 01:14:06 +0000

    Add photo read

--- src/pages/photo.py
+++ src/pages/photo.py
@@ -1,4 +1,6 @@
 """ Code to home web page """
+import os
+from flask import abort
 
 def add_photo_page(app):
     """ Add Home Page """
@@ -7,3 +9,11 @@ def add_photo_page(app):
     def photo_page(refresh=900):
         """ Photo Page """
         return "<p>Photo page to go here! <br> Refresh is: " + str(refresh) + "</p>"
+
+    @app.route('/photo/<string:filename>')
+    def photo_contents(filename=""):
+        filepath = "/photos/"+filename
+        if filename == "" or len(filename.split()) > 1 or not os.path.isfile(filepath):
+            abort(404)
+        filecontent = open(filepath, "r", encoding="utf-8")
+        return filecontent.read()

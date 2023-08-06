7e6869a15bbead420a089da1e89fc652fed3c805
Thomas Cousin <907403+Tomcuzz@users.noreply.github.com>
2023-08-06 23:15:22 +0000

    Settings page save added

--- src/pages/settings_page.py
+++ src/pages/settings_page.py
@@ -1,10 +1,20 @@
 """ Code to home web page """
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from src.helpers.settings import Settings
 
 def add_settings_pages(app, configs:Settings):
     """ Add Settings Page """
-    @app.route("/settings")
+    @app.route("/settings", methods=['GET', 'POST'])
     def settings_page():
         """ Settings Page """
+        if (request.method == 'POST' and
+            request.form['photo_location'] != "" and
+            request.form['user'] != "" and
+            request.form['pass'] != ""):
+            configs.photo_location = request.form['photo_location']
+            configs.username = request.form['user']
+            configs.password = request.form['pass']
+            configs.save_settings()
+            return redirect(url_for('home_page'))
+
         return render_template('settings.html', Configs=configs)

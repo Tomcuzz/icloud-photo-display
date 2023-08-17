cc70fbcaeb9b12296fc0ed88fa9c22e3b8e41382
tomcuzz <thomas.cousin@me.com>
2023-08-17 23:40:31 +0200

    Fix page metric names

--- src/helpers/metrics.py
+++ src/helpers/metrics.py
@@ -2,8 +2,8 @@ from prometheus_client import Counter, Gauge
 
 class Metrics(object):
     def __init__(self):
-        self.counter__requests__photo_page = Counter('photo_requests', 'Number of photo page requests')
-        self.counter__error__404 = Counter('404_counter', 'Number of 404 page returned')
+        self.counter__requests__photo_page = Counter('page_photo_requests', 'Number of photo page requests')
+        self.counter__error__404 = Counter('page_error_404_counter', 'Number of 404 page returned')
         self.gauge__icloud__token_exparation_seconds = Gauge("icloud_token_exparation_seconds", "Number of seconds till icloud 2fa token expires")
         self.gauge__icloud__token_exparation_epoch = Gauge("icloud_token_exparation_epoch", "Epoch of when icloud 2fa token expires")
 

edad0aee87b02e3747962bdbde9e30354a99371c
tomcuzz <thomas.cousin@me.com>
2023-08-18 18:42:02 +0200

    Sync thread refactored

--- src/helpers/sync_thread.py
+++ src/helpers/sync_thread.py
@@ -1,4 +1,5 @@
 from time import sleep
+import logging
 from threading import Thread, Lock
 from src.helpers.icloud import ICloud # pylint: disable=import-error
 from src.helpers.settings import Settings # pylint: disable=import-error
@@ -9,9 +10,14 @@ class SyncThreadHandler(object):
         self.sync_trigger = PeriodicSyncFire(configs, self)
         self.sync_trigger.start()
 
-    def start_sync(self):
-        if not self.sync_runner.sync_lock.locked():
+    def start_sync_if_not_running(self) -> bool:
+        logging.warning("checking if sync alive")
+        if not self.sync_runner.is_alive():
+            logging.warning("triggering sync")
             self.sync_runner.start()
+            return True
+        else:
+            return False
 
 class PeriodicSyncFire(Thread):
     def __init__(self, configs:Settings, sync_handler:SyncThreadHandler):
@@ -29,9 +35,9 @@ class PeriodicSyncFire(Thread):
 class SyncThread(Thread):
     def __init__(self, icloud:ICloud):
         super().__init__()
-        self.sync_lock = Lock()
         self.icloud_connection = icloud
 
     def run(self):
-        with self.sync_lock:
-            self.icloud_connection.sync()
+        logging.warning("starting sync")
+        self.icloud_connection.sync()
+        logging.warning("finished sync")

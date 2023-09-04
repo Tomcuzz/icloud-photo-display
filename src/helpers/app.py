beffc70defd64efd3cf76b06c93b24a5e5c9174e
tomcuzz <thomas.cousin@me.com>
2023-09-05 00:32:19 +0200

    Cirtular dependancy fix

--- src/helpers/app.py
+++ src/helpers/app.py
@@ -1,11 +1,11 @@
-""" Code to run icloud photo display """
-from src.helpers.metrics import Metrics
-from src.helpers.settings import Settings
-from src.helpers.icloud import ICloud
-from src.helpers.sync_thread import SyncHandler
+""" Code Referance all the helpers """
 
 class AppHelper(object):
     def __init__(self):
+        from src.helpers.metrics import Metrics
+        from src.helpers.settings import Settings
+        from src.helpers.icloud import ICloud
+        from src.helpers.sync_thread import SyncHandler
         self.prom_metrics = Metrics()
         self.configs = Settings("/icloudpd", "configs.json")
         self.icloud_helper = ICloud(self.configs, self.prom_metrics)

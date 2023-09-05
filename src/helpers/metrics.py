ba7385ce5991f32fa249bb353848c6e843e2f663
tomcuzz <thomas.cousin@me.com>
2023-09-05 17:00:55 +0200

    grammer fix

--- src/helpers/metrics.py
+++ src/helpers/metrics.py
@@ -9,7 +9,7 @@ class Metrics(object):
         self.gauge__icloud__token_exparation_epoch = Gauge("icloud_token_exparation_epoch", "Epoch of when icloud 2fa token expires")
 
         self.gauge__icloud__last_sync_seconds = Gauge("icloud_last_sync_seconds", "Number of seconds since the last sync", ["SyncName"])
-        self.gauge__icloud__last_sync_epoch = Gauge("icloud_last_sync_epoch", "Epoch since the last sync", ["SyncName"])
+        self.gauge__icloud__last_sync_epoch = Gauge("icloud_last_sync_epoch", "Epoch of the last sync", ["SyncName"])
         self.gauge__icloud__last_sync_elapse_time = Gauge("icloud_sync_elapse_time", "Number of seconds the sync took", ["SyncName"])
 
         self.counter__icloud__number_of_files_downloaded = Counter("icloud_number_of_files_downloaded", "Number of files the sync downloaded")

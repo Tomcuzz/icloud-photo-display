5cd0d1b4965ea83585619cdcbeb2ddd45360bba4
Thomas Cousin <907403+Tomcuzz@users.noreply.github.com>
2023-12-06 15:31:31 +0000

    Update app.py

--- src/app.py
+++ src/app.py
@@ -12,18 +12,6 @@ from src.helpers.app import AppHelper
 
 app = Flask(__name__)
 
-app_helper = AppHelper(app)
-
-home_page.add_home_page(app, app_helper)
-photo_page.add_photo_page(app, app_helper)
-sync_status.add_sync_status_pages(app, app_helper)
-settings_page.add_settings_pages(app, app_helper)
-
-# Add prometheus wsgi middleware to route /metrics requests
-app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
-    '/metrics': make_wsgi_app()
-})
-
 LOG_LEVEL = str(os.getenv("LOG_LEVEL", "INFO"))
 if LOG_LEVEL == "DEBUG":
     print("Logging set to: " + LOG_LEVEL)
@@ -43,3 +31,15 @@ elif LOG_LEVEL == "CRITICAL":
 else:
     print("ERROR: Unknown Logging level, level set to: INFO")
     app.logger.setLevel(logging.INFO)
+
+app_helper = AppHelper(app)
+
+home_page.add_home_page(app, app_helper)
+photo_page.add_photo_page(app, app_helper)
+sync_status.add_sync_status_pages(app, app_helper)
+settings_page.add_settings_pages(app, app_helper)
+
+# Add prometheus wsgi middleware to route /metrics requests
+app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
+    '/metrics': make_wsgi_app()
+})

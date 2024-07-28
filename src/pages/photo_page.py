ebb58a14a9add3e1e4ed23a82ed1df3137433e1c
Thomas Cousin <thomas.cousin@me.com>
2024-07-28 23:11:01 +0100

    App log fix

--- src/pages/photo_page.py
+++ src/pages/photo_page.py
@@ -30,7 +30,7 @@ def add_photo_page(app, app_helper:AppHelper):
         disk_photos = paths.get_files_on_disk(app_helper.configs.photo_location)
         if filename == "random": # If we set random as string get one randomly from disk
             filename = random.choice(list(disk_photos.keys()))
-            app.flask_app.logger.info("Random Image providing: " + download_path)
+            app_helper.flask_app.logger.info("Random Image providing: " + download_path)
         if  filename not in disk_photos:
             app_helper.prom_metrics.counter__error__404.inc()
             abort(404)

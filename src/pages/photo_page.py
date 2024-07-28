27cdc37cf54b64add6091b3acd1fb16e1e86ee1b
Thomas Cousin <thomas.cousin@me.com>
2024-07-28 23:04:00 +0100

    Add limit size functions on output

--- src/pages/photo_page.py
+++ src/pages/photo_page.py
@@ -2,7 +2,7 @@
 import os
 import io
 import random
-from flask import render_template, abort, send_file
+from flask import render_template, abort, send_file, request
 from wand.image import Image
 from wand.resource import limits
 from src.helpers.app import AppHelper # pylint: disable=import-error
@@ -30,12 +30,17 @@ def add_photo_page(app, app_helper:AppHelper):
         disk_photos = paths.get_files_on_disk(app_helper.configs.photo_location)
         if filename == "random": # If we set random as string get one randomly from disk
             filename = random.choice(list(disk_photos.keys()))
+            app.flask_app.logger.info("Random Image providing: " + download_path)
         if  filename not in disk_photos:
             app_helper.prom_metrics.counter__error__404.inc()
             abort(404)
         if not os.path.isfile(disk_photos[filename]['file_path']):
             app_helper.prom_metrics.counter__error__404.inc()
             abort(404)
+
+        width = request.args.get('width', default = -1, type = int)
+        height = request.args.get('height', default = -1, type = int)
+
             
         # Use 1GB of ram before writing temp data to disk.
         limits['memory'] = 1024 * 1024 * 1024
@@ -43,6 +48,8 @@ def add_photo_page(app, app_helper:AppHelper):
         limits['width'] = 1000000
         limits['height'] = 1000000
         with Image(filename=disk_photos[filename]['file_path']) as img:
+            if width > 0 and height > 0:
+                img.transform(resize=f"{width}x{height}>")
             return send_file(
                 io.BytesIO(img.make_blob('jpeg')),
                 mimetype='image/jpeg',
